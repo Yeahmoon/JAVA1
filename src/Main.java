b1125517ee6b86965a817cf247cd231ff8c4c588
Ekaterina Khalillulina <yeahmoon@hotmail.com>
2021-12-13 17:42:29 +0300

    scr added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int CurrentBalance = 2_000_000_000;
+        int TransferAmount = 500_000_000;
+        int total = CurrentBalance + TransferAmount;
+        System.out.println(total);
+
+    }
+}
